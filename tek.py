3d34f3d62a98b3b28182054ea026c2c7a8254cd6
WesleyLewis05 <wjl4jj@virginia.edu>
2024-10-25 17:58:00 -0400

    device config feature added

--- tek.py
+++
@@ -1,9 +0,0 @@
-import serial.tools.list_ports
-
-def list_serial_ports():
-    ports = serial.tools.list_ports.comports()
-    for port in ports:
-        print(f"Port: {port.device}, Description: {port.description}, HWID: {port.hwid}")
-
-if __name__ == "__main__":
-    list_serial_ports()
